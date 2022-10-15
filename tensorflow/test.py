92ac5aefcee4f9cdd1a9d34079f4f1b2513d0ab1
Aamod Save <56135585+Aamod1996@users.noreply.github.com>
2022-10-15 15:51:42 -0700

    Added docker configs for tensorflow and cpp

---
+++ tensorflow/test.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import tensorflow_model_optimization as tfmot
+import tensorflow_recommenders as tfrs
+import tensorflow_datasets as tfds
+import tensor2tensor
+import tensorflow_io as tfio
+import tensorflow_probability as tfp
+import sonnet as snt
+import mesh_tensorflow
+import tensorflow_text as tf_text
+import numpy as np
+import pandas as pd
+import sklearn
+from PIL import Image
+import h5py
+import scipy
+import seaborn
+import tf_agents
